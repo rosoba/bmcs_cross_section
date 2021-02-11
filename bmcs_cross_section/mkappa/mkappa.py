76a2bad3f8f14311f758af3fb36824066ef6bb9c
saeed <saeed.rastegarian@rwth-aachen.de>
2021-02-11 01:00:38 +0100

    shrinkage notebook initiated

--- bmcs_cross_section/mkappa/mkappa.py
+++ bmcs_cross_section/mkappa/mkappa.py
@@ -55,13 +55,13 @@ class MKappaSymbolic(SymbExpr):
     eps_top_solved = {eps_top: sp.solve(kappa + eps_z_.diff(z), eps_top)[0]}
     eps_z = eps_z_.subs(eps_top_solved)
 
-    # steel_material_factor = 1. / 1.15
-    # carbon_material_factor = 1. / 1.5
-    # concrete_material_factor = 0.85 / 1.5
+    steel_material_factor = 1. / 1.15
+    carbon_material_factor = 1. / 1.5
+    concrete_material_factor = 0.85 / 1.5
 
-    steel_material_factor = 1.
-    carbon_material_factor = 1.
-    concrete_material_factor = 1.
+#     steel_material_factor = 1.
+#     carbon_material_factor = 1.
+#     concrete_material_factor = 1.
 
     sig_c_eps = concrete_material_factor * sp.Piecewise(
         (0, eps < eps_cu),
