665f6cf49ccfe08fd813f3025dc5736712919fa6
ishomam <ishomam@gmail.com>
2021-06-26 23:06:30 +0200

    Solution of eps_bot is mostly stable now

--- bmcs_cross_section/mkappa/mkappa.py
+++ bmcs_cross_section/mkappa/mkappa.py
@@ -162,6 +162,33 @@ class MKappa(InteractiveModel, InjectSymbExpr):
         N_c_t = self.get_N_c_t(kappa_t, eps_bot_t)
         return N_c_t + N_s_t
 
+    # All following functions ending by "_single" are for solving eps_bot with scaler values as input and were used for
+    # debugging (TODO: delete them later when code is robust)
+    # def get_N_t_single(self, kappa, eps_bot):
+    #     N_s = np.sum(self.get_N_s_j_single(kappa, eps_bot))
+    #     N_c = self.get_N_c_t_single(kappa, eps_bot)
+    #     return N_c + N_s
+    #
+    # def get_N_s_j_single(self, kappa, eps_bot):
+    #     # get the strain at the height of the reinforcement
+    #     eps_z_j = self.symb.get_eps_z(np.full_like(self.z_j, kappa), np.full_like(self.z_j, eps_bot), self.z_j)
+    #     # Get the crack bridging force in each reinforcement layer
+    #     # given the corresponding crack-bridge law.
+    #     N_s_j = np.array([r.get_N(eps_z) for r, eps_z in zip(self.cross_section_layout.items, eps_z_j.T)], dtype=np.float_)
+    #     return N_s_j
+    #
+    # def get_N_c_t_single(self, kappa, eps_bot):
+    #     b_z_m = self.cross_section_shape_.get_b(self.z_m)
+    #     N_z_m2 = b_z_m * self.get_sig_c_z_single(np.full_like(self.z_m, kappa), np.full_like(self.z_m, eps_bot), self.z_m.T)
+    #     return np.trapz(N_z_m2, x=self.z_m)
+    #
+    # def get_sig_c_z_single(self, kappa, eps_bot, z_m):
+    #     """Get the stress profile over the height"""
+    #     eps_z = self.symb.get_eps_z(np.full_like(z_m, kappa), np.full_like(z_m, eps_bot), z_m)
+    #     sig_c_z = self.matrix_.get_sig(eps_z)
+    #     return sig_c_z
+
+
     # SOLVER: Get eps_bot to render zero force
 
     # num_of_trials = tr.Int(30)
@@ -198,20 +225,42 @@ class MKappa(InteractiveModel, InjectSymbExpr):
     #         raise SolutionNotFoundError('No solution', res.message)
     #     return res.x
 
+    solve_for_eps_bot_pointwise = Bool(True)
 
     @tr.cached_property
     def _get_eps_bot_t(self):
-        # One by one solution for kappa values
-        res = np.array([root(lambda eps_bot: self.get_N_t(np.array([kappa]), eps_bot), np.array([0.0000001]),
-                             tol=1e-6).x[0] for kappa in self.kappa_t])
-        return res
+        if self.solve_for_eps_bot_pointwise:
+            """ INFO: Instability in eps_bot solutions was caused by unsuitable init_guess value causing a convergence 
+            to non-desired solutions. Solving the whole kappa_t array improved the init_guess after each
+            calculated value, however, instability still there. The best results were obtained by taking the last 
+            solution as the init_guess for the next solution like in the following.. """
+            # One by one solution for kappa values
+            res = []
+            init_guess = 0.00001
+            for kappa in self.kappa_t:
+                sol = root(lambda eps_bot: self.get_N_t(np.array([kappa]), eps_bot), np.array([init_guess]), tol=1e-6).x[0]
 
-        # Array solution for the whole kappa_t
-        # res = root(lambda eps_bot_t: self.get_N_t(self.kappa_t, eps_bot_t),
-        #            0.0000001 + np.zeros_like(self.kappa_t), tol=1e-6)
-        # if not res.success:
-        #     print('No solution', res.message)
-        # return res.x
+                # Using get_N_t_single which get scaler values for debugging (TODO: to be deleted later)
+                # sol = root(lambda eps_bot: self.get_N_t_single(kappa, eps_bot), init_guess, tol=1e-6).x[0]
+
+                # This condition is to avoid having init_guess~0 which causes non-convergence
+                if sol > 1e-5:
+                    init_guess = sol
+                res.append(sol)
+            res = np.array(res)
+
+            # res = np.array([root(lambda eps_bot: self.get_N_t(np.array([kappa]), eps_bot), np.array([0.0000001]),
+            #                      tol=1e-6).x[0] for kappa in self.kappa_t])
+            # res = np.array([root(lambda eps_bot: self.get_N_t_single(kappa, eps_bot), 0.0000001,
+            #                      tol=1e-6).x[0] for kappa in self.kappa_t])
+            return res
+        else:
+            # Array solution for the whole kappa_t
+            res = root(lambda eps_bot_t: self.get_N_t(self.kappa_t, eps_bot_t),
+                       0.0000001 + np.zeros_like(self.kappa_t), tol=1e-6)
+            if not res.success:
+                print('No solution', res.message)
+            return res.x
 
     # POSTPROCESSING
 
