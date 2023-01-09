4fa6f9a954e1daa3dd70ec1f1d66c23030b5fff0
ishomam <ishomam@gmail.com>
2023-01-09 09:54:37 +0100

    EC2 shrinkage + creep added

--- bmcs_cross_section/api.py
+++ bmcs_cross_section/api.py
@@ -2,6 +2,7 @@ from bmcs_cross_section.mkappa.mkappa import MKappa, MKappaParamsStudy
 from bmcs_cross_section.matmod import \
     ConcreteMatMod, EC2PlateauConcreteMatMod, EC2ConcreteMatMod, PWLConcreteMatMod, SteelReinfMatMod, CarbonReinfMatMod
 from bmcs_cross_section.matmod.ec2 import EC2
+from bmcs_cross_section.matmod.ec2_creep_shrinkage import EC2CreepShrinkage
 # from bmcs_cross_section.pullout import PullOutModel1D
 # from bmcs_cross_section.pullout.pullout_sim import PullOutModel
 from bmcs_cross_section.cs_design import BarLayer, ReinfLayer, FabricLayer, CrossSectionLayout, \
